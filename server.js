a78342a0a418f9ac0765ab27248f290226372147
amiravni16 <amiravni13@gmail.com>
2025-10-28 18:34:18 +0200

    Fix CORS to allow production frontend domain

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(express.json())
 const corsOptions = {
     origin: [
         process.env.FRONTEND_URL,
+        'https://photograph-frontend.onrender.com',
         'http://127.0.0.1:3000',
         'http://localhost:3000',
         'http://127.0.0.1:5173',
